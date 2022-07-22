c37030afe17584f50e4c5e7fe26ebe51df67ff21
ChuYeonBin <cndusqls98@gmail.com>
2022-07-22 11:07:23 +0900

    chore: font 정의

--- src/styles/globals.js
+++ src/styles/globals.js
@@ -5,7 +5,7 @@ const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-    font-family: Open-Sans, Helvetica, Sans-Serif;
+    font-family: Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif;
   }
 
   ul {
