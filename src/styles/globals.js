8ea8f891e49b0816641d19902c9b17631c0c2123
ChuYeonBin <cndusqls98@gmail.com>
2022-07-22 11:04:41 +0900

    chore: global style 정의

---
+++ src/styles/globals.js
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: Open-Sans, Helvetica, Sans-Serif;
+  }
+
+  ul {
+    list-style: none;
+  }
+
+  a {
+    text-decoration: none;
+    color: black;
+  }
+`;
+
+export default GlobalStyle;
